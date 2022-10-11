02f53fb4de08915d00cf43eec78c6bd9b5720c05
MostovayaE <113229730+MostovayaE@users.noreply.github.com>
2022-10-11 22:45:17 +0300

    Update script5.js

--- script5.js
+++ script5.js
@@ -1,18 +1,18 @@
-function onClick() {
-  alert("click");
-}
-
 window.addEventListener('DOMContentLoaded', function (event) {
   console.log("DOM fully loaded and parsed");
+  function onClick() {
+    let f1 = document.getElementById("field1").value;
+    let f2 = document.getElementById("field2").value;
+    let r = document.getElementById("result");
+    let onlydigitnotzero = /^(?!(0))\d+$/;
+    if (f1.match(onlydigitnotzero) === null || f2.match(onlydigitnotzero) === null) {
+      alert("Введено не число или начинается с нуля");
+      r.innerHTML = "Ошибка";
+    }
+    else {
+      r.innerHTML = parseInt(f1) * parseInt(f2);
+    }
+  }
   let b = document.getElementById("button1");
   b.addEventListener("click", onClick);
 });
-
-function click1() {
-
-  let f1 = document.getElementsByName("field1");
-  let f2 = document.getElementsByName("field2");
-  let r = document.getElementById("result");
-  r.innerHTML = parseInt(f1[0].value) * parseInt(f2[0].value);
-  return false;
-  }
